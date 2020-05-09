74849e96e1546314f8fd22c38aa70db03f015de7
AliShawk <59791386+AliShawk@users.noreply.github.com>
2020-05-09 04:22:42 +0200

    add comment

--- data/src/main/java/com/example/data/model/Movie.kt
+++ data/src/main/java/com/example/data/model/Movie.kt
@@ -2,6 +2,12 @@ package com.example.data.model
 
 import com.google.gson.annotations.SerializedName
 
+/**
+ * I am thinking that each layer should have there data models.
+ * If the models are not different from layer to layer we can use the same models.
+ * Instead I created different data models for each layer to show case how we can make our mapping.
+ */
+
 data class Movie(
     @SerializedName("title")
     var title: String,
